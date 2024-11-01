9c5f53576575a48eed82b8630af35d3a41158077
Real-pine <crewger9030@gmail.com>
2024-11-01 13:11:22 +0900

    Update Player.cs

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -9,6 +9,8 @@ public class Player : MonoBehaviour
 
     private void Awake()
     {
+        // 이 오브젝트가 없어지고 새로운 캐릭터가 생성되더라도 charactermanager랑 이어지기때문에 데이터가 날라가지 않음
+        // 그러니까 처음부터 
         CharacterManager.Instance.Player = this;
         controller = GetComponent<PlayerInputSystem>();
         condition = GetComponent<PlayerCondition>();
